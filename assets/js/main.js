1d2507511b31bba4e206ef83e663c3b168e5f61d
Sanjay Salve <sanjay.SHVinfo@gmail.com>
2023-10-30 10:14:48 +0530

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,13 @@ $(document).ready(function () {
   });
 });
 
-// active
-
+// // active
+// $(document).ready(function(){
+//   $('ul li a').click(function(){
+//     $('li a').removeClass("active");
+//     $(this).addClass("active");
+// });
+// });
 
 
 // profile
